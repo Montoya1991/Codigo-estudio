d21ea0f6ecb7e93138ec3d498f0c14689a27bd8c
Montoya <jdmontoya0491@hotmail.com>
2022-03-22 15:04:57 -0500

    Visual iniciado

---
+++ codigos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace codigos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // prueba de conexion 
+        }
+    }
+}
